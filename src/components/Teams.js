7537fb150ee8138063efeedeee5e4cf8bac7c664
Rodrigo Johann Reckziegel Nunes <hannziegel@gmail.com>
2022-09-06 19:51:48 -0300

    Add search bar

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getTeams } from '../redux/teams';
 import Team from './Team';
@@ -7,18 +7,24 @@ import '../styles/Teams.css';
 
 const Teams = () => {
   const teamsList = useSelector((state) => state.teams.teams);
+  const [search, setSearch] = useState('');
   const dispatch = useDispatch();
   useEffect(() => {
     if (!teamsList.length) dispatch(getTeams());
   }, []);
 
+  const filterTeams = teamsList.filter(
+    (teams) => !search || new RegExp(search.toLowerCase(), '').test(teams.strTeam.toLowerCase()),
+  );
+
   return (
     <div className="page-container container">
       <Header />
       <div className="section-title">Teams in NBA</div>
+      <input type="search" value={search} onChange={(event) => { setSearch(event.target.value); }} className="searchBar" placeholder="Search..." />
       <div className="teams-container">
         {
-          teamsList.map((team) => (
+          filterTeams.map((team) => (
             <Team
               key={team.strTeam}
               team={team}
