4376c939851a3fc92dcf19ab718d85c7806412f1
Rodrigo Johann Reckziegel Nunes <hannziegel@gmail.com>
2022-09-08 01:10:31 -0300

    Add TeamMatch test

---
+++ src/__tests__/TeamMatch.test.js
@@ -0,0 +1,29 @@
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import React from 'react';
+import store from '../Redux/configureStore';
+import TeamMatch from '../components/TeamMatch';
+
+describe('App js testing', () => {
+  const teamLastGames = {
+    strHomeTeam: 'PropTypes.string',
+    intHomeScore: 'PropTypes.string',
+    strAwayTeam: 'PropTypes.string',
+    intAwayScore: 'PropTypes.string',
+    strThumb: 'PropTypes.string',
+  };
+
+  test('Renders App', () => {
+    const tree = render(
+      <React.StrictMode>
+        <Provider store={store}>
+          <Router>
+            <TeamMatch teamLastGames={teamLastGames} />
+          </Router>
+        </Provider>
+      </React.StrictMode>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+});
