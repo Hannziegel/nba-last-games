54ea472ce5d332f715363ed19b160259f821bcfa
Rodrigo Johann Reckziegel Nunes <hannziegel@gmail.com>
2022-09-08 01:08:57 -0300

    Add Header test

---
+++ src/__tests__/Header.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { render } from '@testing-library/react';
+import store from '../redux/configureStore';
+import Header from '../components/Header';
+
+describe('Team test', () => {
+  test('snapshot for Team', () => {
+    const tree = render(
+      <React.StrictMode>
+        <Provider store={store}>
+          <Header />
+        </Provider>
+      </React.StrictMode>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+});
