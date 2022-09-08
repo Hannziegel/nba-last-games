73d44e7efe8880e7c4c0edfaa90999443fc628c0
Rodrigo Johann Reckziegel Nunes <hannziegel@gmail.com>
2022-09-08 01:09:45 -0300

    Add Teams test

---
+++ src/__tests__/Teams.test.js
@@ -0,0 +1,21 @@
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import React from 'react';
+import store from '../Redux/configureStore';
+import Teams from '../components/Teams';
+
+describe('App js testing', () => {
+  test('Renders App', () => {
+    const tree = render(
+      <React.StrictMode>
+        <Provider store={store}>
+          <Router>
+            <Teams />
+          </Router>
+        </Provider>
+      </React.StrictMode>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+});
